e6242c9c40653a252f3bebff4725fd2fe052fe8f
Mental <Alekov99@mail.ru>
2024-05-07 00:05:19 +0500

    Update g buffer albedo specular precision

--- MentalOpenGLEngine/src/Graphics/GFrameBuffer.cpp
+++ MentalOpenGLEngine/src/Graphics/GFrameBuffer.cpp
@@ -44,7 +44,7 @@ void GFrameBuffer::Create(int width, int height)
 
 	glGenTextures(1, &mAlbedoSpecularTextureId);
 	glBindTexture(GL_TEXTURE_2D, mAlbedoSpecularTextureId);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, width, height, 0, GL_RGBA, GL_FLOAT, NULL);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
